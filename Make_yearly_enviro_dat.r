24225b3dc78058e476f2456e0837bc855b46dbae
sarahsupp <sarah@weecology.org>
2014-10-01 16:58:41 -0500

    added pathnames and spacing to this file, still need to update rdata file before it can be used

--- Make_yearly_enviro_dat.r
+++ Make_yearly_enviro_dat.r
@@ -1,19 +1,24 @@
+# code to make the yearly climate data file from the raw data, 
+# to be used in the models for the extreme limits manuscript results
+# Code originally written by Pieter Beck, revised by Sarah R. Supp
 
-#load("C:\\Users\\pbeck.WHRC\\Dropbox\\Hummers\\Hummer_code\\P4_Extreme_events_Broad_tailed\\Results\\annual.anom.df.rdata")
-load("A:\\Share\\pbeck\\Hummer_NASA\\Code_copy\\annual.anom.df.rdata")
+datpath <- "/Users/sarah/Documents/github/extreme_limits/data/"
 
-yearly.climdat <- function(tapply.out,nm){
-  tapply.out <-as.data.frame(tapply.out);tapply.out <- cbind.data.frame(tapply.out,as.numeric(rownames(tapply.out)));
-  colnames(tapply.out)[2]<-"yr"
-  colnames(tapply.out)[1]<- nm  
+load(paste(datpath, "annual.anom.df.rdata", sep=""))
+
+yearly.climdat <- function(tapply.out, nm){
+  tapply.out <-as.data.frame(tapply.out)
+  tapply.out <- cbind.data.frame(tapply.out, as.numeric(rownames(tapply.out)))
+  colnames(tapply.out)[2] <- "yr"
+  colnames(tapply.out)[1] <- nm  
   return(tapply.out)
 }
-#debug(yearly.climdat)
+
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
 #calculate mean Tes during winter and append ####
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
-yearly.Tes <- tapply(annual.anom.df$Tes,annual.anom.df$BaseYear.x,mean)
+yearly.Tes <- tapply(annual.anom.df$Tes, annual.anom.df$BaseYear.x, mean)
 yearly.Tes <- yearly.climdat(yearly.Tes,nm="Tes")
 yearly.climate <- yearly.Tes
 #yearly.catch.stat <- merge(yearly.catch.stat,yearly.Tes)
@@ -29,9 +34,9 @@ yearly.climate <- yearly.Tes
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
 #calculate mean Tmin during winter and append ####
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
-yearly.Tmin <- tapply(annual.anom.df$tmin,annual.anom.df$BaseYear.x,mean)
-yearly.Tmin <- yearly.climdat(yearly.Tmin,nm="Tmin")
-yearly.climate <- merge(yearly.climate,yearly.Tmin)
+yearly.Tmin <- tapply(annual.anom.df$tmin, annual.anom.df$BaseYear.x, mean)
+yearly.Tmin <- yearly.climdat(yearly.Tmin, nm="Tmin")
+yearly.climate <- merge(yearly.climate, yearly.Tmin)
 #yearly.catch.stat <- merge(yearly.catch.stat,yearly.Tes)
 
 # #+++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -47,66 +52,68 @@ yearly.climate <- merge(yearly.climate,yearly.Tmin)
 #calculate day 10%, 25% and 50% quantiles of Te
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
 #which quantile does 10C represent?
-Te.10C.quant <- function(x){100*length(which(x<=10))/length(x)}
-yearly.Te.10C.q <- tapply(annual.anom.df$Te,annual.anom.df$BaseYear.x,Te.10C.quant)
-yearly.Te.10C.q <- yearly.climdat(yearly.Te.10C.q,nm="yearly.Te.10C.q")
-yearly.climate <- merge(yearly.climate,yearly.Te.10C.q)
+Te.10C.quant <- function(x){ 100 * length(which(x <= 10)) / length(x) }
+yearly.Te.10C.q <- tapply(annual.anom.df$Te, annual.anom.df$BaseYear.x, Te.10C.quant)
+yearly.Te.10C.q <- yearly.climdat(yearly.Te.10C.q,nm = "yearly.Te.10C.q")
+yearly.climate <- merge(yearly.climate, yearly.Te.10C.q)
 rm(yearly.Te.10C.q)
 
 
 #what's the 10% quantile?
-yearly.Te.10C.quant <- tapply(annual.anom.df$Te,annual.anom.df$BaseYear.x,Te.10C.quant)
+yearly.Te.10C.quant <- tapply(annual.anom.df$Te, annual.anom.df$BaseYear.x, Te.10C.quant)
 
-Quant10perc <- function(x){quantile(x,probs=0.10)}
-yearly.Te.q10 <- tapply(annual.anom.df$Te,annual.anom.df$BaseYear.x,Quant10perc)
-yearly.Te.q10 <- yearly.climdat(yearly.Te.q10,nm="yearly.Te.q10")
+Quant10perc <- function(x){quantile(x, probs=0.10)}
+yearly.Te.q10 <- tapply(annual.anom.df$Te, annual.anom.df$BaseYear.x, Quant10perc)
+yearly.Te.q10 <- yearly.climdat(yearly.Te.q10, nm = "yearly.Te.q10")
 yearly.climate <- merge(yearly.climate,yearly.Te.q10)
 rm(yearly.Te.q10)
 
-Quant25perc <- function(x){quantile(x,probs=0.25)}
-yearly.Te.q25 <- tapply(annual.anom.df$Te,annual.anom.df$BaseYear.x,Quant25perc)
-yearly.Te.q25 <- yearly.climdat(yearly.Te.q25,nm="yearly.Te.q25")
-yearly.climate <- merge(yearly.climate,yearly.Te.q25)
+Quant25perc <- function(x){quantile(x, probs=0.25)}
+yearly.Te.q25 <- tapply(annual.anom.df$Te, annual.anom.df$BaseYear.x, Quant25perc)
+yearly.Te.q25 <- yearly.climdat(yearly.Te.q25, nm="yearly.Te.q25")
+yearly.climate <- merge(yearly.climate, yearly.Te.q25)
 rm(yearly.Te.q25)
 
-Quant50perc <- function(x){quantile(x,probs=0.5)}
-yearly.Te.q50 <- tapply(annual.anom.df$Te,annual.anom.df$BaseYear.x,Quant50perc)
-yearly.Te.q50 <- yearly.climdat(yearly.Te.q50,nm="yearly.Te.q50")
-yearly.climate <- merge(yearly.climate,yearly.Te.q50)
+Quant50perc <- function(x){quantile(x, probs=0.5)}
+yearly.Te.q50 <- tapply(annual.anom.df$Te, annual.anom.df$BaseYear.x, Quant50perc)
+yearly.Te.q50 <- yearly.climdat(yearly.Te.q50, nm="yearly.Te.q50")
+yearly.climate <- merge(yearly.climate, yearly.Te.q50)
 rm(yearly.Te.q50)
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
 #calculate mean NDVI and precip during winter and append ####
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
-yearly.NDVI <- tapply(annual.anom.df$smoothNDVI,annual.anom.df$BaseYear.x,mean)
+yearly.NDVI <- tapply(annual.anom.df$smoothNDVI, annual.anom.df$BaseYear.x, mean)
 yearly.NDVI <- yearly.climdat(yearly.NDVI,nm="NDVI")
 yearly.climate <- merge(yearly.climate,yearly.NDVI)
-yearly._1NDVI <- yearly.NDVI; yearly._1NDVI$minNDVI <- -1*yearly._1NDVI$NDVI
+yearly._1NDVI <- yearly.NDVI
+yearly._1NDVI$minNDVI <- -1*yearly._1NDVI$NDVI
 yearly.climate <- merge(yearly.climate,yearly._1NDVI)
 
-yearly.pre <- tapply(annual.anom.df$pre,annual.anom.df$BaseYear.x,mean)*60*60*6
+yearly.pre <- tapply(annual.anom.df$pre, annual.anom.df$BaseYear.x, mean) * 60 * 60 * 6
 yearly.pre <- yearly.climdat(yearly.pre,nm="pre")
 yearly.climate <- merge(yearly.climate,yearly.pre)
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
 #calculate day 133 NDVI and cummprecip and append ####
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++
-winterend.NDVI <- annual.anom.df[annual.anom.df$DOYsinceBase.x==133,]
-winterend.NDVI <- tapply(winterend.NDVI$smoothNDVI,winterend.NDVI$BaseYear.x,mean)  
-winterend.NDVI <- yearly.climdat(winterend.NDVI,nm="winterNDVI")
-yearly.climate <- merge(yearly.climate,winterend.NDVI)
+winterend.NDVI <- annual.anom.df[annual.anom.df$DOYsinceBase.x == 133, ]
+winterend.NDVI <- tapply(winterend.NDVI$smoothNDVI, winterend.NDVI$BaseYear.x, mean)  
+winterend.NDVI <- yearly.climdat(winterend.NDVI, nm = "winterNDVI")
+yearly.climate <- merge(yearly.climate, winterend.NDVI)
 
-winterend._1NDVI <- winterend.NDVI; winterend._1NDVI$minwinterNDVI <- -1*winterend._1NDVI$winterNDVI
-yearly.climate <- merge(yearly.climate,winterend._1NDVI)
+winterend._1NDVI <- winterend.NDVI 
+winterend._1NDVI$minwinterNDVI <- -1*winterend._1NDVI$winterNDVI
+yearly.climate <- merge(yearly.climate, winterend._1NDVI)
 
-winterend.cumpre <- annual.anom.df[annual.anom.df$DOYsinceBase.x==133,]
-winterend.cumpre <- tapply(winterend.cumpre$cumpre,winterend.cumpre$BaseYear.x,mean)  
-winterend.cumpre <- yearly.climdat(winterend.cumpre,nm="wintercumpre")
-yearly.climate <- merge(yearly.climate,winterend.cumpre)
+winterend.cumpre <- annual.anom.df[annual.anom.df$DOYsinceBase.x == 133, ]
+winterend.cumpre <- tapply(winterend.cumpre$cumpre, winterend.cumpre$BaseYear.x, mean)  
+winterend.cumpre <- yearly.climdat(winterend.cumpre, nm="wintercumpre")
+yearly.climate <- merge(yearly.climate, winterend.cumpre)
 
 #the year in yearly.climate denotes the second year of that winter, rather than the
 #baseyear !! to conform with the capture data from Arizona !
-yearly.climate$yr <- yearly.climate$yr+1
+yearly.climate$yr <- yearly.climate$yr + 1
 
 save(yearly.climate,
      file="C:\\Users\\pbeck.WHRC\\Dropbox\\Hummers\\Hummer_code\\P4_Extreme_events_Broad_tailed\\Results\\yearly.climate.rdata")
