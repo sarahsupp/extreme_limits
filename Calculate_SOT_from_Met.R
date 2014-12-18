21789949ab38a04952a15f5833df876c1c885e9d
sarahsupp <sarah@weecology.org>
2014-12-18 14:01:53 -0600

    replaced Calculate_SOT and calculate_diffuse with correct versions which we found in PAB folder called 'code_copy'

--- Calculate_SOT_from_Met.R
+++ Calculate_SOT_from_Met.R
@@ -1,17 +1,6 @@
-# Diffuse_fraction_of_solar_radiation.R
-#
-# Pieter Beck (psabeck@gmail.com)
-# 09-12-2013
-# 
-#Calculate Standard Operative T for hummingbirds using meteo input data 
+#functions to calculate Standard Operative T for hummingbirds
+#using meteo input data 
 #based on Don Powers's email "Estimating Operative Temperature"
-#
-#
-# Changelog:
-#   VERS  | DATE    	 | CHANGES				| BY
-#   ------|------------|----------------|---- 
-#		1.0.0	|	09-12-2013 | Wrote script		| PB
-
 
 #vARIABLES TO GET FROM METEO
 # Ta          ambient T (Kelvin)
@@ -27,103 +16,96 @@
 #u wind speed
 #NCEP var table:
 #http://rda.ucar.edu/datasets/ds094.0/#metadata/grib2.html?_do=y
-
-ra.calc <- function(u){
-  #caclulate aerodynamic resistance to convective heat transfer
-  #using an equation below was estimated for a small sparrow
-  # Args:
-  #  u: wind speed in m/s
-  #
-  # Returns:
-  #  aerodynamic resistance to convective heat transfer (s m-1)
-  # 
-  ra <- 37.76*u^(-0.495)
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####  
+#caclulate aerodynamic resistance to convective heat transfer
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####  
+ra.calc <- function(u #wind speed in m/s
+                    ){
+  # ra=aerodynamic resistance to convective heat transfer (s m-1)
+  # the equation below was estimated for a small sparrow
+  ra <- 37.76*(u^(-0.495))
   return(ra)
 }  
-
-Rabs.calc <- function(Sp, ApOVERA = 0.25, Sd, aS = 0.89, aL = 0.9, Li){
-  #Calculate long + shortwave radiation absorbed
-  #
-  # Args:
-  #  Sp: direct shortwave radiation (W m^(-2))
-  #  ApOVERA: ratio of projected shadow area to total surface area. 
-  #           This is basically used to measure how much of the surface area is 
-  #           directly absorbing shortwave radiation. 0.25 is a conservative starting estimate
-  #   Sd: diffuse shortwave radiation. This is usually measured with a shadow radiometer
-  #   aS: mean shortwave absorptivity. 0.89 was estimated for 2 bird species
-  #   aL: mean longwave absorptivity. Don will find value 0.9 is place holder !!!!!!
-  #   Li: incoming longwave radiation
-  
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+#calculate long + shortwave radiation absorbed
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+Rabs.calc <- function(Sp, #direct shortwave radiation (W m^(-2))
+                      ApOVERA = 0.25, #ratio of projected shadow area to total surface area. 
+                      #This is basically used to measure how much of the surface area is 
+                      #directly absorbing shortwave radiation. 0.25 is a conservative starting estimate
+                      Sd , #diffuse shortwave radiation. This is usually measured with a shadow radiometer
+                      aS = 0.89, #mean shortwave absorptivity. 0.89 was estimated for 2 bird species
+                      aL = 0.97,#mean longwave absorptivity.Walsberg 1992
+                      Li #incoming longwave radiation
+                      ){
   Rabs.shortwave <- ((Sp*ApOVERA) + Sd) * aS
   Rabs.longwave <-  Li * aL
+
   Rabs <- Rabs.shortwave + Rabs.longwave
   return(Rabs)
 }
-
-Te.calc <- function(Ta, Rabs, sigma = 5.67e-8, epsilon = 0.95, rhoCp = 1200, ra){
-  #Calculate operative temperature
-  #
-  # Args:
-  #  Ta: ambient T
-  #  Rabs: long-wave+shortwave radiation absorbed (W m-2)
-  #  sigma: 5.67e-8, i.e. Stefan Boltman constant W m^(-2) K^(-4)
-  #  epsilon: 3missivity of the bird's surface
-  #  rhoCp: product of the density and specific heat capacity of air (J m-3 K^(-1))
-  #  ra: aerodynamic resistance to convective heat transfer (s m-1)
-  #
-  # Returns:
-  #  Operative temperature
-  #
-  if (Ta < 263){cat("STOP !!! Ta needs to be provided in K !!!\n");
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+#calculate operative temperature
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+Te.calc <- function(Ta, #ambient T
+                    Rabs, # long-wave+shortwave radiation absorbed (W m-2)
+                    sigma = 5.67e-8, #Stefan Boltman constant W m^(-2) K^(-4)
+                    epsilon = 0.95, #Emissivity of the bird's surface
+                    rhoCp = 1200, #product of the density and specific heat capacity of air (J m-3 K^(-1))
+                    ra # aerodynamic resistance to convective heat transfer (s m-1)
+                    ){
+  if (any(Ta < 253)){cat("STOP !!! Ta needs to be provided in K !!!\n");
                 cat("provided Ta range is: ",range(Ta,na.rm=T),"\n");browser()}
-  # Te <- Ta + (Rabs - sigma*epsilon*(Ta^4))/(rhoCp / 4*sigma*(Ta^3) + ra) #old equation - does not match Greek et al. 1989
-  Te <- Ta + (Rabs - sigma*epsilon*(Ta^4)) / (rhoCp/ra + 4*sigma*epsilon*(Ta^3))
+  Te <- Ta + (Rabs - sigma*epsilon*(Ta^4))/(rhoCp /ra + (4*sigma*epsilon*(Ta^3)))
   return(Te)
 }
-
-Tes.calc <- function(Te, Tb=315, u){
-  #Calculate standard operative temperature
-  #
-  # Args
-  #   Te: Operative temperature
-  #   Tb: 42+273, #42 C, or 42 + 273 K
-  #   u: wind speed in m/s)
-  #
-  # Returns
-  #   Standard Operative Temperature
-  Tes <- Tb - (1 + (0.26 * sqrt(u))*(Tb-Te))
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+#calculate standard operative temperature
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+Tes.calc <- function(Te,
+                     Tb = 42+273, #42 C, or 42 + 273 K
+                     u # wind speed in m/s)
+                    ){
+  Tes <- Tb - ((1 + (0.26 * sqrt(u)))*(Tb-Te))
   return(Tes)
+  #return((0.26 * sqrt(u))*(Tb-Te))
 }
-
-Tes.calc.compl <- function(Ta, u, Sp, Sd, Li){
-  #A comprensive function to estimate standard operative temperature from basic meteo. variables,
-  #but processed radiation data
-  #
-  # Args:
-  #   Ta: ambient T (Kelvin)
-  #   u: wind speed (m/s)
-  #   Sp: direct shortwave radiation (W m^(-2)) 
-  #   Sd: diffuse shortwave radiation (W m^(-2)) 
-  #   Li: incoming longwave radition (W m^(-2))
-  #
-  # Returns:
-  #   Standard Operative Temperature
-  #
+#test
+#  Te. <-  273+c(0:40)
+#  u. <- rep(1,41)
+#  Tes. <- Tes.calc(Te=Te.,u=u.)
+#  plot(Te.-273,Tes.-273);abline(0,1)
+# 
+# (1/.26)^2
+# Tes. <- Tes.calc(Te=Te.,u=(1/.26)^2)
+# plot(Te.-273,Tes.-273);abline(0,1)
+# 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+#a first "complete" function to estimate standard operative temperature
+#drawing on the functions above
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
+Tes.calc.compl <- function(Ta,
+                           u,
+                           Sp,
+                           Sd,
+                           Li
+                          ){
   #calc aerodynamic resistance to convective heat transfer
   ra <- ra.calc(u=u)
   #calc longwave+shortwave radiation absorbed
   Rabs <- Rabs.calc(Sp=Sp, Sd=Sd, Li=Li) 
   Te <- Te.calc(Ta=Ta,ra=ra,Rabs=Rabs)
   Tes <- Tes.calc(Te,u=u)
-  return(Tes)
+  win.graph();plot(Ta-273,Te-273,pch=16,cex=.1,col=rgb(.1,.1,.1,.025),xlim=range(c(Ta-273,Te-273),na.rm=T),ylim=range(c(Ta-273,Te-273),na.rm=T));abline(0,1)  
+  win.graph();plot(Te-273,Tes-273,pch=16,cex=.1,col=rgb(.1,.1,.1,.025),xlim=range(c(Tes-273,Te-273),na.rm=T),ylim=range(c(Tes-273,Te-273),na.rm=T));abline(0,1)  
+  return(cbind.data.frame(Te,Tes))
 }
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
 # source the code to calculate diffuse and direct irradiation components
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++####
 
 #source("C:\\Users\\pbeck.WHRC\\Dropbox\\Hummers\\Hummer_code\\P4_Extreme_events_Broad_tailed\\calculate_diffuse_fraction.R")
-#source("C:\\share\\pbeck\\Hummer_NASA\Code_copy\\calculate_diffuse_fraction.R")
-source("C:/Users/sarah/Documents/GitHub/extreme_limits/calculate_diffuse_fraction.R")
+source("C:\\Users/sarah/Documents/GitHub/extreme_limits/calculate_diffuse_fraction.R")
 #provides the function
 #SpSd.calc(Rsurface,Rclearsky,solarzen)
   
@@ -138,16 +120,6 @@ Tes.calc.compl.incl.rad <- function(Ta,
                            R_extra_terr,
                            solarzen                                  
                   ){
-#A comprensive function to estimate standard operative temperature from basic meteo. variables,
-#and raw radiation data (drawing on functions in Diffuse_fraction_of_solar_radiation.r)
-#
-# Args:
-#   Ta: ambient T (Kelvin)
-#   u: wind speed (m/s)
-#   Li: incoming longwave radition (W m^(-2))
-#   RSurface:
-#   R_extra_terr:
-#   solarzen
                     SpSd <- SpSd.calc(Rsurface=Rsurface,R_extra_terr=R_extra_terr,solarzen=solarzen)
                     win.graph();par(mfrow=c(2,2))
                     hist(Rsurface);hist(R_extra_terr);hist(SpSd[,1]);hist(SpSd[,2])
